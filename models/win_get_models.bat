7fa71f748b13caa475f6e478792e86fc6c6e525b
LosCrossos <165311345+loscrossos@users.noreply.github.com>
2025-06-02 11:53:40 +0200

    Update win_get_models.bat typo

--- models/win_get_models.bat
+++ models/win_get_models.bat
@@ -1,4 +1,4 @@
-git clone --depth 1 --filter=blob:none git clone https://huggingface.co/Zyphra/Zonos-v0.1-transformer
+git clone --depth 1 --filter=blob:none https://huggingface.co/Zyphra/Zonos-v0.1-transformer
 rmdir /Q/S Zonos-v0.1-transformer/.git
 
 
